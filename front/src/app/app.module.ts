558516ce6c914fd27f78d22c6eb26473dd0b9f6e
Richartrex <learic7@gmail.com>
2019-07-16 22:50:27 +0200

    create pages prices, school, contact, galerie

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,18 +5,57 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HttpClientModule } from '@angular/common/http';
+import { PerformancesComponent } from './performances/performances.component';
+import { RouterModule, Routes } from "@angular/router";
+import { PricesComponent } from './prices/prices.component';
+import { SchoolComponent } from './school/school.component';
+import { ContactComponent } from './contact/contact.component';
+import { GalerieComponent } from './galerie/galerie.component';
+
+const routes: Routes = [
+    
+  // {
+  //   path: "admin", pathMatch: "full", component: AdministrationComponent
+  // },
+  // {
+  //   path : '' , redirectTo : '/home' , pathMatch:'full'
+  // },
+  // { path: "home", component: FrontComponent, children: 
+  //  [
+      { path: "home", pathMatch: "full", component: HomeComponent },
+      { path: "performances", pathMatch: "full", component: PerformancesComponent },
+      // { path: "prices", pathMatch: "full", component: PricesComponent },
+      // { path: "school", pathMatch: "full", component: SchoolComponent },
+      // { path: "contact", pathMatch: "full", component: ContactComponent },
+      // { path: "galerie", pathMatch: "full", component: GalerieComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    PerformancesComponent,
+    PricesComponent,
+    SchoolComponent,
+    ContactComponent,
+    GalerieComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
-  ],
+    FormsModule,
+    RouterModule.forRoot(
+      routes,      
+      {
+        useHash: false,
+        enableTracing: false,
+        anchorScrolling: "enabled",
+        onSameUrlNavigation: "reload",
+        scrollPositionRestoration: "enabled",
+      }
+      ) 
+    ],  
   providers: [],
   bootstrap: [AppComponent]
 })
