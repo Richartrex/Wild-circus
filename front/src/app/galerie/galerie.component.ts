36843fb4785d6e8966c220336638da772e7f211b
Richartrex <learic7@gmail.com>
2019-07-17 17:44:49 +0200

    Picture back toward front ok

--- front/src/app/galerie/galerie.component.ts
+++ front/src/app/galerie/galerie.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalerieComponent implements OnInit {
 
+  display:boolean = false;
+  modalsrc = "";
   constructor() { }
 
   ngOnInit() {
+  }
+
+  modal(event:Event):void{
+    let img:HTMLImageElement = event.target as HTMLImageElement
+    this.modalsrc = img.src;
+    this.toggleModal();
+  }
+
+  toggleModal():void{
+    let modalcontainer:HTMLElement = document.getElementById("myModal");
+    this.display = !this.display;
+
+    if( this.display ){
+      modalcontainer.style.display = "block";
+    }
+    else{
+      modalcontainer.style.display = "none";
+    }
   }
 
 }
